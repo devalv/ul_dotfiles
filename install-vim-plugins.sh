698d94d5aeabb33e52337fd2e3554099797ec624
Aleksei <63240599+devalv@users.noreply.github.com>
2021-04-28 16:09:03 +0300

    Update install-vim-plugins.sh

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -8,7 +8,7 @@ rm -rf ~/.vim/bundle
 echo "installing vim plugin manager"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "installing vim plugins. press enter..."
+echo "installing vim plugins"
 echo | echo | vim +PluginInstall +qall &>/dev/null
 
 exit 0
