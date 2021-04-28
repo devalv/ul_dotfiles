4d03213a21e885ad36eb7601d0f2fca02e6da665
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-04-28 17:46:13 +0300

    omf installation updates

--- install-omf.sh
+++ install-omf.sh
@@ -2,14 +2,14 @@
 
 set -e
 
+echo "set fish as default shell"
+echo /usr/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/bin/fish
+
 echo "installing omf"
 curl -L https://get.oh-my.fish | fish
 
 echo "update omf plugins"
 fish -c omf update
-
-echo "set fish as default shell"
-echo /usr/bin/fish | sudo tee -a /etc/shells
-chsh -s /usr/bin/fish
 
 exit 0
