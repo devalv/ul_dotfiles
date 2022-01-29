6e82a8fbc40ff36a313af8186ae0487ba6716cc4
Aleksei <63240599+devalv@users.noreply.github.com>
2022-01-29 19:27:02 +0300

    Update install-snap-utils.sh

--- install-snap-utils.sh
+++ install-snap-utils.sh
@@ -4,6 +4,7 @@ set -e
 
 [ -z "$(which dbeaver-ce)" ] && snap install dbeaver-ce
 [ -z "$(which pycharm-professional)" ] && snap install pycharm-professional --classic
+[ -z "$(which goland)" ] && snap install goland --classic
 [ -z "$(which insomnia)" ] && snap install insomnia
 [ -z "$(which telegram-desktop)" ] && snap install telegram-desktop
 [ -z "$(which vscode)" ] && snap install code --classic
